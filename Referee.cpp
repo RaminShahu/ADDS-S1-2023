8d91b5379e9a13b99e7d6d0c1b02518d800d61e9
RaminShahu <raminshahu786@gmail.com>
2023-03-01 14:29:02 +1030

    Commit

--- Referee.cpp
+++ Referee.cpp
@@ -10,16 +10,16 @@ Referee::Referee()
 
 }
 Player* Referee::refGame(Player *player1, Player *player2) {
-    if (player1->move == player2->move) 
+    
+    if (player1->move == 'R' && player2->move == 'R') 
     {
-      return player1;
+      return nullptr;
     }
-    if (player1->move == 'R' && player2->move == 'S') {
-      return player1;
-    } else if (player1->move == 'S' && player2->move == 'P') {
-      return player1;
-    } else if (player1->move == 'P' && player2->move == 'R') {
-      return player1;
+    else if (player1->move == 'S' && player2->move == 'R') 
+    {
+      return player2;
+    } else if (player1->move == 'R' && player2->move == 'P') {
+      return player2;
     }
     return player1;
 }
